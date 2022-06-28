42f039c680d16af07a2849fe79b868c169a966b8
B I Mohammed Abbas <the10minoverview@gmail.com>
2022-06-28 19:26:13 +0530

    Add files via upload

---
+++ FactorsSum.c
@@ -0,0 +1,36 @@
+/*
+Program to multiply two numbers without using arithmetic operators
+By: B I Mohammmed Abbas
+Github: https://github.com/biabbas
+*/
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+int Add(int a, int b){
+   bool c = 0,x,y;
+  for(int i=0;(i<=(int)floor(log2(b)+1)) || c!=0;i++){
+    x = (a&(1<<i))!=0;
+    y = (b&(1<<i))!=0;
+    if(c^x^y)
+    a = a | (1<<i);// Set ith bit of a to 1
+    else
+    a = a & (~(1<<i));// Reset ith bit of a to 0
+    c =(c&(x^y) | x&y);
+  }
+  printf("RES: %d\n",a);
+  return a;
+}
+int Mul(int a,int b){
+    int res = 0;
+    for(int i = 0;i<=(int)floor(log2(b)+1);i++)
+    if(b&(1<<i))
+    res = Add((a<<i),res);
+return res;
+}
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+printf("a * b = %d\n",Mul(a,b));
+
+    return 0;
+}
